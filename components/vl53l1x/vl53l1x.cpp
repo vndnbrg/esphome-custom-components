050f8af0c153535b3e0460c31bf745e65cd9182b
vndnbrg <ralfprzybilla@gmail.com>
2026-03-28 18:25:21 +0100

    Fix: replace legacy 4-arg I2C calls with new 3-arg API (size_t casts)

--- components/vl53l1x/vl53l1x.cpp
+++ components/vl53l1x/vl53l1x.cpp
@@ -924,7 +924,7 @@ bool VL53L1XComponent::vl53l1x_write_bytes_16(uint8_t a_register, const uint16_t
   std::unique_ptr<uint16_t[]> temp{new uint16_t[len]};
   for (size_t i = 0; i < len; i++)
     temp[i] = i2c::htoi2cs(data[i]);
-  return (this->write_register16(a_register, reinterpret_cast<const uint8_t *>(temp.get()), len * 2, true) == i2c::ERROR_OK);
+  return (this->write_register16(a_register, reinterpret_cast<const uint8_t *>(temp.get()), static_cast<size_t>(len * 2)) == i2c::ERROR_OK);
 }
 
 bool VL53L1XComponent::vl53l1x_write_byte(uint16_t a_register, uint8_t data) {
